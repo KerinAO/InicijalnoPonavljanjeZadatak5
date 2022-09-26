fed02eeab53fe0b036eb594bc5d94081d81c085d
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-09-26 10:45:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicjalnoPonavljanjeZadatak5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url;
+
+            Console.WriteLine("Upišite URL adresu:");
+            url = Console.ReadLine();
+
+            if (url.StartsWith("www.") && url.EndsWith(".hr"))
+            {
+                Console.WriteLine("URL je pravilan.");
+            }
+            else
+            {
+                Console.WriteLine("URL nije pravilan.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
